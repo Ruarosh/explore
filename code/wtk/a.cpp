bf099446e863ddf59585800c31487ea3aaa19e1d
wei.ju <wei.ju@aispeech.com>
2019-04-19 14:09:44 +0800

    cpp update

--- code/wtk/a.cpp
+++ code/wtk/a.cpp
@@ -2,30 +2,25 @@
 
 using namespace std;
 
-class A
+class SingletonStatic
 {
+private:
+        static const SingletonStatic* m_instance;
+        SingletonStatic(){}
 public:
-  A ():m_iVal(0){test();}
-  virtual void func() { std::cout<<"base\t" << m_iVal<<endl;}
-  void test(){func();}
-public:
-int m_iVal;
+        static const SingletonStatic* getInstance()
+        {   
+                return m_instance;
+        }   
 };
 
-class B : public A
-{
-public:
-  B(){test();}
-  virtual void func()
-  {
-    ++m_iVal;
-    std::cout<<"derived\t" << m_iVal<<endl;
-  }
-};
+//外部初始化 before invoke main
+SingletonStatic* SingletonStatic::m_instance = NULL;
 
 int main(int argc ,char* argv[])
 {
-  A*p = new B;
-  p->test();
+    SingletonStatic::m_instance = 
+
+
   return 0;
 }
--- code/wtk/singleton.cpp
+++ code/wtk/singleton.cpp
@@ -1,36 +1,68 @@
 /*
+单例模式
+懒汉版（Lazy Singleton）：单例实例在第一次被使用时才进行初始化，这叫做延迟初始化。
+饿汉版（Eager Singleton）：指单例实例在程序运行时被立即执行初始化
+
 https://segmentfault.com/a/1190000015950693
 https://www.cnblogs.com/liyuan989/p/4264889.html
+https://songlee24.github.io/2014/03/11/singleton-pattern/
+http://blog.jobbole.com/96754/
 */
 
-// DCL, double-checked locking
-template<typename T>
+// 懒汉模式
+// ==========静态成员实例===========
 class Singleton
 {
-public:
-    static T& getInstance()
-    {
-        if(!value_)
-        {
-            MutexGuard guard(mutex_);
-            if (!value_)
-            {
-                value_ = new T();
-            }
-        }
-        return *value_;
-    }
- 
 private:
-    Singleton();
-    ~Singleton();
- 
-    static T*     value_;
-    static Mutex  mutex_;
+        static Singleton* m_instance;
+        Singleton(){}
+public:
+        static Singleton* getInstance();
 };
  
-template<typename T>
-T* Singleton<T>::value_ = NULL;
- 
-template<typename T>
-Mutex Singleton<T>::mutex_;
+Singleton* Singleton::getInstance()
+{
+        if(NULL == m_instance)
+        {
+                Lock();//借用其它类来实现，如boost
+                if(NULL == m_instance)
+                {
+                        m_instance = new Singleton;
+                }
+                UnLock();
+        }
+        return m_instance;
+}
+
+
+// ==========内部静态成员实例===========
+class SingletonInside
+{
+private:
+        SingletonInside(){}
+public:
+        static SingletonInside* getInstance()
+        {
+                Lock(); // not needed after C++0x
+                static SingletonInside instance;
+                UnLock(); // not needed after C++0x
+                return instance; 
+        }
+};
+
+
+//饿汉模式
+class SingletonStatic
+{
+private:
+        static const SingletonStatic* m_instance;
+        SingletonStatic(){}
+public:
+        static const SingletonStatic* getInstance()
+        {
+                return m_instance;
+        }
+};
+
+//外部初始化 before invoke main
+const SingletonStatic* SingletonStatic::m_instance = new SingletonStatic;
